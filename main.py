915008c7ee0ea17a5aaecafb4f5a73af767dd4be
AarkeTripathi <aarke2002tripathi@gmail.com>
2024-09-14 20:41:02 +0530

    CORS

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import uvicorn
 import model
 
 app=FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload_pdf/")
 async def upload_pdf(file: UploadFile = File(...)):
